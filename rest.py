18a5b0f6b59e0cf09e3d1ba8afa25eb1056caa25
nb-sommerprosjekt <auwalsoe@gmail.com>
2017-08-29 13:57:59 +0200

    Added logging of results, time, input etc

--- rest.py
+++ rest.py
@@ -5,13 +5,15 @@ from flask_cors import CORS
 from html2text import html2text
 
 import classifier
-
+import preprocessor
+import time
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/rest_doc/<path:url>', methods = ['GET', 'POST'])
+@app.route('/rest_doc/<path:url>', methods = ['GET'])
 def read_text_url(url):
+    start = time.time()
+    log_file = open("log_file.txt", "a")
 
     url = url.replace("%3A",":")
     url_decoded = url.replace("%2F","/")
@@ -24,7 +26,17 @@ def read_text_url(url):
     test_string="Penger er kult, og finans og poteter"
     #res =classifier.run_classification(str(clean))
     res = classifier.run_classification(clean)
+    total_time = time.time()- start
+    log_file.write("url:::"+str(url_decoded)+"\n"+"\n" +
+                   str(res)+"\n"+"\n"
+                   +"Tid brukt:"+str(total_time)+"\n"+"\n"
+                   +"Tekst brukt til klassifisering:"+'\n'+'\n'
+                   +str(preprocessor.text_to_clean_stemmed_text(streng,False)))
+
     return res
+
+
+
 
 
 if __name__ == '__main__':
