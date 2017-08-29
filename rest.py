ad9600f769233ed24ed9e89e729254c4f65ecdbd
nb-sommerprosjekt <auwalsoe@gmail.com>
2017-08-29 14:35:04 +0200

    Updated format of logfile

--- rest.py
+++ rest.py
@@ -7,12 +7,14 @@ from html2text import html2text
 import classifier
 import preprocessor
 import time
+import datetime
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/rest_doc/<path:url>', methods = ['GET'])
 def read_text_url(url):
     start = time.time()
+    st = datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S')
     log_file = open("log_file.txt", "a")
 
     url = url.replace("%3A",":")
@@ -23,15 +25,14 @@ def read_text_url(url):
     streng = soup.get_text()
     streng.encode('utf8')
     clean = html2text(streng)
-    test_string="Penger er kult, og finans og poteter"
-    #res =classifier.run_classification(str(clean))
     res = classifier.run_classification(clean)
     total_time = time.time()- start
-    log_file.write("url:::"+str(url_decoded)+"\n"+"\n" +
-                   str(res)+"\n"+"\n"
+    log_file.write("Tidspkt:"+str(st)+'\n\n'+"url:::"
+                   +str(url_decoded)+"\n"+"\n"
+                   +str(res)+"\n"+"\n"
                    +"Tid brukt:"+str(total_time)+"\n"+"\n"
                    +"Tekst brukt til klassifisering:"+'\n'+'\n'
-                   +str(preprocessor.text_to_clean_stemmed_text(streng,False)))
+                   +str(preprocessor.text_to_clean_stemmed_text(clean,False)))
 
     return res
 
